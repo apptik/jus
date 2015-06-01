6bbed6e7fa9808adb056f3ee351ab2edca26a475
Kalin Maldzhanski <djodjo@djodjo.org>
2015-06-01 17:22:05 +0200

    update gradle + libs

--- examples/src/main/java/org/djodjo/comm/jus/examples/MainActivity.java
+++ examples/src/main/java/org/djodjo/comm/jus/examples/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +13,7 @@ import android.view.MenuItem;
 import org.djodjo.comm.jus.examples.nav.NavigationDrawerFragment;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     /**
