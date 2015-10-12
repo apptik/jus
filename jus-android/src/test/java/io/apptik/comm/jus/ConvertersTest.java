1c5662d3aead51fe1689f9e66c7f44cdf29d1ce6
Kalin Maldzhanski <djodjo@djodjo.org>
2015-10-12 10:22:31 +0200

    use roboelectric

--- jus-android/src/test/java/io/apptik/comm/jus/ConvertersTest.java
+++ jus-android/src/test/java/io/apptik/comm/jus/ConvertersTest.java
@@ -2,12 +2,18 @@ package io.apptik.comm.jus;
 
 import org.json.JSONObject;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import io.apptik.comm.jus.converter.JSONConverter;
 import io.apptik.comm.jus.http.HTTP;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21, manifest = "src/main/AndroidManifest.xml")
 public class ConvertersTest {
 
     @Test
@@ -17,7 +23,6 @@ public class ConvertersTest {
         JSONConverter.Factory factory = new JSONConverter.Factory();
         Converter<JSONObject, NetworkRequest> converter1 =
                 (Converter<JSONObject, NetworkRequest>) factory.toRequest(JSONObject.class, null);
-       // System.out.println("val: " + val);
         NetworkRequest request = converter1.convert(val);
         NetworkResponse response = new NetworkResponse.Builder()
                 .setBody(request.data)
@@ -27,8 +32,7 @@ public class ConvertersTest {
                 (Converter<NetworkResponse, JSONObject>) factory.fromResponse(JSONObject.class, null);
 
         JSONObject val2 = converter2.convert(response);
-        //System.out.println("val2: " + val2);
-        assertThat(val2).isEqualTo(val);
+        assertThat(val2.toString()).isEqualTo(val.toString());
     }
 
 }
