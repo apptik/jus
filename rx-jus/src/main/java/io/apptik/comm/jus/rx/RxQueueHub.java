32bcd43994d57d39c44583cdec5524e4003ad447
Kalin Maldzhanski <djodjo@djodjo.org>
2016-08-09 19:36:58 +0200

    use rxHub az a base for RxQueueHub

--- rx-jus/src/main/java/io/apptik/comm/jus/rx/RxQueueHub.java
+++ rx-jus/src/main/java/io/apptik/comm/jus/rx/RxQueueHub.java
@@ -9,38 +9,36 @@ import io.apptik.comm.jus.rx.event.JusEvent;
 import io.apptik.comm.jus.rx.event.MarkerEvent;
 import io.apptik.comm.jus.rx.event.ResultEvent;
 import io.apptik.comm.jus.rx.request.RxRequest;
+import io.apptik.rxhub.AbstractRxHub;
 import rx.Observable;
 import rx.functions.Func1;
-import rx.subjects.Subject;
 
 
 /**
  * More specific RequestQueue related RxHub.
  * It grabs all requests and sorts them in Subjects by their Tag if set
  */
-public class RxQueueHub extends RxHub {
+public class RxQueueHub extends AbstractRxHub {
 
     public RxQueueHub(RequestQueue queue) {
-        super(true);
-        queue.addListenerFactory(new RequestListener.SimpleListenerFactory() {
-            @Override
-            public RequestListener.MarkerListener getMarkerListener(Request<?> request) {
-                Object rTag = request.getTag();
-                if (rTag != null) {
-                    Subject subj = RxQueueHub.this.get(request.getTag());
-                    if (subj != null) {
-                        RxRequest.allEventsObservable(request).subscribe(subj);
-                    } else {
-                        RxQueueHub.this.put(rTag, RxRequest.allEventsObservable(request));
+        queue.addListenerFactory(
+                new RequestListener.SimpleListenerFactory() {
+                    @Override
+                    public RequestListener.MarkerListener getMarkerListener
+                            (Request<?> request) {
+                        Object rTag = request.getTag();
+                        if (rTag != null) {
+                            RxQueueHub.this.addProvider(rTag, RxRequest
+                                    .allEventsObservable(request));
+                        }
+                        return null;
                     }
                 }
-                return null;
-            }
-        });
+        );
     }
 
     public Observable getMarkers(Object tag) {
-        return super.get(tag).filter(new Func1<JusEvent, Boolean>() {
+        return super.getNode(tag).filter(new Func1<JusEvent, Boolean>() {
             @Override
             public Boolean call(JusEvent jusEvent) {
                 return jusEvent instanceof MarkerEvent;
@@ -49,7 +47,7 @@ public class RxQueueHub extends RxHub {
     }
 
     public Observable getResults(Object tag) {
-        return super.get(tag).filter(new Func1<JusEvent, Boolean>() {
+        return super.getNode(tag).filter(new Func1<JusEvent, Boolean>() {
             @Override
             public Boolean call(JusEvent jusEvent) {
                 return jusEvent instanceof ResultEvent;
@@ -58,11 +56,21 @@ public class RxQueueHub extends RxHub {
     }
 
     public Observable getErrors(Object tag) {
-        return super.get(tag).filter(new Func1<JusEvent, Boolean>() {
+        return super.getNode(tag).filter(new Func1<JusEvent, Boolean>() {
             @Override
             public Boolean call(JusEvent jusEvent) {
                 return jusEvent instanceof ErrorEvent;
             }
         });
+    }
+
+    @Override
+    public NodeType getNodeType(Object tag) {
+        return NodeType.BehaviorRelay;
+    }
+
+    @Override
+    public boolean isNodeThreadsafe(Object tag) {
+        return true;
     }
 }
