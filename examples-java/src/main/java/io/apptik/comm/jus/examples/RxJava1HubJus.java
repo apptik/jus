be04a299a0e2afe6f8c829bed3aacc13d5228e19
Kalin Maldzhanski <djodjo@djodjo.org>
2016-12-20 22:36:07 +0100

    fix java examples

--- examples-java/src/main/java/io/apptik/comm/jus/examples/RxJava1HubJus.java
+++ examples-java/src/main/java/io/apptik/comm/jus/examples/RxJava1HubJus.java
@@ -12,13 +12,13 @@ import io.apptik.comm.jus.converter.Converters;
 import io.apptik.comm.jus.http.HttpUrl;
 import io.apptik.comm.jus.rx.RxQueueHub;
 import io.apptik.comm.jus.rx.event.JusEvent;
-import io.apptik.rxhub.RxHub;
+import io.apptik.rhub.RxJava1Hub;
 import rx.Observer;
 
-public class RxHubJus {
+public class RxJava1HubJus {
     public static void main(String[] args) {
         RequestQueue queue = Jus.newRequestQueue(new File("."));
-        RxHub beerHub = new RxQueueHub(queue);
+        RxJava1Hub beerHub = new RxQueueHub(queue);
         Set<String> opts = new HashSet<>();
         if (args != null) {
             Collections.addAll(opts, args);
@@ -32,7 +32,7 @@ public class RxHubJus {
             );
         }
 
-        beerHub.getNodeFiltered("beer", JusEvent.class).subscribe(new Observer<JusEvent>() {
+        beerHub.getPub("beer", JusEvent.class).subscribe(new Observer<JusEvent>() {
 
             //should not happen
             @Override
